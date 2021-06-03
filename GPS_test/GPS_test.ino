9e61972ef3d2cfb2dc523bcffe24434a2462bd0a
Brychan Thomas <brychan.thomas@btinternet.com>
2021-06-03 15:16:28 +0100

    Measured NEO-6M power consumption and added in comment

--- GPS_test/GPS_test.ino
+++ GPS_test/GPS_test.ino
@@ -49,6 +49,8 @@ void loop() {
   }
 }
 
+// normally (when trying to obtain fix) the NEO-6M module seems to use around 50mA
+// but in this mode it uses around 11mA
 void gpsOff() {
   uint8_t ubx[] = {0xB5, 0x62, 0x02, 0x41, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x4D, 0x3B};
   for(int i=0; i<16; i++) {
